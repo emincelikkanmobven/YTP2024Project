1970dbf24e356f1c32d956d117a5aad5e175628d
emincelikkanmobven <emin.celikkan@mobven.com>
2024-04-24 19:58:05 +0300

    test commit

---
+++ YTP2024Project/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  YTP2024Project
+//
+//  Created by Emin Çelikkan on 24.04.2024.
+//
+
+import UIKit
+
+class TestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
