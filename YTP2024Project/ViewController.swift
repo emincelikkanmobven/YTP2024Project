7efe3f5d98db435f6089368293d4cf75e7f6552c
emincelikkanmobven <emin.celikkan@mobven.com>
2024-04-24 17:32:01 +0300

    Initial Commit

---
+++ YTP2024Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YTP2024Project
+//
+//  Created by Emin Çelikkan on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
